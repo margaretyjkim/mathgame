ff14a9b50543dc1200ce26608d88783c07b9d69e
margaretyjkim <margaret.yj.kim@gmail.com>
2015-12-24 22:10:16 -0500

    changed the buttons to mybuttons

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,3 +1,33 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
 
+// var counter = 100;
+// var t;
+// var isTimerOn = false;
+
+// function countdown()
+// {
+// 	document.getElementById("txt").value = counter;
+// 	counter--;
+// 	t = setTimeout("countdown();", 1000);
+// }
+
+// function stopMe()
+// {
+// 	isTimerOn = false;
+// 	clearTimeout(t);
+// }
+
+// function startMe()
+// {
+// 	if (!isTimerOn)
+// 	{
+// 		isTimeOn = true;
+// 		countdown();
+// 	}
+// }
+
+
+// $(window).load(function () {
+// 	startMe;
+// });
