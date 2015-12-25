a8b4c1eb6b67e668212cb527f213d73d2a0a46b8
margaretyjkim <margaret.yj.kim@gmail.com>
2015-12-25 10:15:30 -0500

    separated js file from the layout page to questions js

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,31 +1,31 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 
-// var counter = 100;
-// var t;
-// var isTimerOn = false;
+var counter = 10;
+var t;
+var isTimerOn = false;
 
-// function countdown()
-// {
-// 	document.getElementById("txt").value = counter;
-// 	counter--;
-// 	t = setTimeout("countdown();", 1000);
-// }
+function countdown()
+{
+	document.getElementById("txt").value = counter;
+	counter--;
+	t = setTimeout("countdown();", 1000);
+}
 
-// function stopMe()
-// {
-// 	isTimerOn = false;
-// 	clearTimeout(t);
-// }
+function stopMe()
+{
+	isTimerOn = false;
+	clearTimeout(t);
+}
 
-// function startMe()
-// {
-// 	if (!isTimerOn)
-// 	{
-// 		isTimeOn = true;
-// 		countdown();
-// 	}
-// }
+function startMe()
+{
+	if (!isTimerOn)
+	{
+		isTimeOn = true;
+		countdown();
+	}
+}
 
 
 // $(window).load(function () {
