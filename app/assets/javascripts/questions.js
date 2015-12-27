ac256587af297b44a5a5a50a018f08884a6f10c0
margaretyjkim <margaret.yj.kim@gmail.com>
2015-12-27 06:45:02 -0500

    set up simple setinterval to test things out

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,6 +1,13 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 
+//testing out setInterval
+	function simpleMessage() {
+		alert("10 secs up!")
+	}
+
+	setInterval(simpleMessage, 10000);
+//testing end
 var counter = 10;
 var t;
 var isTimerOn = false;
